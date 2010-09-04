12f09697476640b1f76a1b15cef775aae1ad80da
Anschel Schaffer-Cohen <anschelsc@gmail.com>
2010-09-04 00:46:46 -0400

    Made things much faster by only looking through text once.

--- pydp.py
+++ pydp.py
@@ -2,29 +2,36 @@
 
 import random
 
-bywords=True
-overlap=3
-target=200
+bywords=False
+overlap=4
+target=2000
 
 file='./alice.txt'
 
-def nextbit(text, using):
-    l=len(text)
-    possible=set()
-    for n in range(len(text)):
-        if text[n]==using[0]:
-            possible.add(n)
-    for offset in range(1,len(using)):
-        for index in possible.copy():
-            if text[(index+offset)%l]!=using[offset]:
-                possible.remove(index)
-    return text[(random.choice(tuple(possible))+len(using))%l]
+def iterate(text):      #iterates "around the corner"
+    for i in range(len(text)-overlap):
+        yield i,tuple(text[i:i+overlap])
+    for i in range(1,overlap):
+        yield len(text)-overlap+i,tuple(text[-overlap+i:]+text[:i])
+
+def buildchunkdic(chunks):      #chunks would normally be returned by iterate()
+    chunkdic={}
+    for chunk in chunks:
+        if chunk[1] in chunkdic:
+            chunkdic[chunk[1]].add(chunk[0])
+        else:
+            chunkdic[chunk[1]]={chunk[0]}
+    return chunkdic
+
+def nextbit(text, chunkdic, search):
+    return text[(random.choice(tuple(chunkdic[search]))+overlap)%len(text)]
 
 def dissociate(text, overlap, target):
     start=random.randrange(len(text)-overlap)
     dissociated=list(text[start:start+overlap])
+    chunkdic=buildchunkdic(iterate(text))
     while len(dissociated)<target:
-        dissociated.append(nextbit(text,dissociated[-overlap:]))
+        dissociated.append(nextbit(text,chunkdic,tuple(dissociated[-overlap:])))
     return dissociated
 
 with open(file) as opened:
